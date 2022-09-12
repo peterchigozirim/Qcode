9407558bf676a76df8bd0c18c90d0dd9600b9a15
peter chigozirim <peterchigozirim>
2022-09-12 16:49:02 +0100

    fixed pageNav

--- src/components/Utility/PageNav.vue
+++ src/components/Utility/PageNav.vue
@@ -7,11 +7,11 @@
               GetUrlQrCode
             </p>
           </div>
-          <div class="md:hidden text-white bg-emerald-600 py-1 cursor-pointer px-3 rounded-md">
+          <div @click="openNav = !openNav" class="md:hidden text-white bg-emerald-600 py-1 cursor-pointer px-3 rounded-md">
             <font-awesome :icon="['fas', 'bars']" />
           </div>
         </div>
-        <div class="fixed md:static md:bg-transparent bg-emerald-500/40 w-full md:w-auto h-screen md:h-auto top-14 py-2 md:p-0 md:top-0 bg-opacity-70 left-0">
+        <div :class="openNav? ' w-full left-0 ': '-left-96'" class="fixed md:static transition-all duration-300 md:bg-transparent bg-emerald-500/40 md:w-auto h-screen md:h-auto top-14 py-2 md:p-0 md:top-0 bg-opacity-70 md:left-0">
             <nav class="flex md:flex-row flex-col gap-2 md:gap-4 w-11/12 md:w-auto mx-auto md:mx-0">
               <RouterLink to="/">Home</RouterLink>
               <RouterLink to="/about">About</RouterLink>
@@ -22,7 +22,13 @@
 </template>
 
 <script>
-
+  export default {
+    data(){
+      return{
+        openNav: false
+      }
+    }
+  }
 </script>
 
 <style>
